7c8421f1f1b832e45393fe08495ba6f81269bf10
Lars Willighagen <lars.willighagen@gmail.com>
2019-05-07 13:06:26 +0200

    Initial demo

---
+++ index.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', async function () {
+  const cam = navigator.getUserMedia
+  const pip = 'pictureInPictureEnabled' in document
+
+  if (!(cam && pip)) {
+    alert(`Features not available: ${[!cam &&  'Webcam', !pip && 'Picture in Picture'].filter(Boolean).join(' and '))}`)
+  }
+
+  video.srcObject = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+  })
+  video.play()
+
+  button.addEventListener('click', async function() {
+    button.disabled = true
+    await video.requestPictureInPicture()
+    button.disabled = false
+  })
+}, false)
